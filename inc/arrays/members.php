beca03e1bdd3756da58f8b4afdfa9f3453da1836
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-08 15:10:26 +0100

    Created array for a member

---
+++ inc/arrays/members.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$members =  array(
+    "Member 1" => array(
+        "Name" => "Name Of The Member",
+        "Title" => "Title Of The Member",
+        "Image" => "/img/members/member1.jpg",
+        "Quote" => "Quote Of The Member",
+        "Linkedin" => "https://www.linkedin.com/in/member1/",
+        "Email" => "member@gmail.com",
+        "Description" => "Description Of The Member",
+        "Website" => "www.member1.be"   
+    ),
+
+    "Member 2" => array(
+        "Name" => "Name Of The Member",
+        "Title" => "Title Of The Member",
+        "Image" => "/img/members/member1.jpg",
+        "Quote" => "Quote Of The Member",
+        "Linkedin" => "https://www.linkedin.com/in/member2/",
+        "Email" => "member@gmail.com",
+        "Description" => "Description Of The Member",
+        "Website" => "www.member2.be"
+    )
+);
+
+
+?>
