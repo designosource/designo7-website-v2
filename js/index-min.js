8ef0c7896683af697d73a2ecdf8e2a360aba26d4
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-15 22:28:32 +0200

    Smooth scroll on internal link

--- js/index-min.js
+++ js/index-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$(function(){$("body a").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=$(this.hash);(t=t.length?t:$("[name="+this.hash.slice(1)+"]")).length&&$("html,body").animate({scrollTop:t.offset().top},300)}})})});
