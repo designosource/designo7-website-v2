827aa717183de418f9403c7b16cc52395747f599
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-18 00:11:35 +0200

    Added favicon

--- team/index.php
+++ team/index.php
@@ -6,7 +6,7 @@
 
     <!-- END GOOGLE ANALYTICS SCRIPT -->
 
-    <link rel="shortcut icon" type="image/x-icon" href="">
+    <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
