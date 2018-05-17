f707197c5c7f66bf9c0b1510cd9ecd8d7fa497af
ChrisOortman <oortmanchris@hotmail.com>
2018-05-17 15:45:06 +0200

    grid template overview members

---
+++ team/overview.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- GOOGLE ANALYTICS SCRIPT -->
+
+    <!-- END GOOGLE ANALYTICS SCRIPT -->
+
+    <link rel="shortcut icon" type="image/x-icon" href="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php include '../inc/head/meta_tags.php'; ?>
+
+    <title>Team | Overview</title>
+
+    <!-- Font(s) for this template (Google font / font-awesome) -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          type="text/css">
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/cooper-hewitt" type="text/css"/>
+
+    <!-- Reset css file -->
+    <link href="../css/reset.css" rel="stylesheet">
+
+    <!-- Animate.css -->
+    <link href="../css/animate.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="../css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<main>
+    <section class="page-container">
+        <div class="previous-page">
+            <a href="#">vorige pagina</a>
+        </div>
+
+        <h1>Meet the team</h1>
+
+        <article class="overview" id="members-grid">
+            <a href="#">
+                <img src="https://media.giphy.com/media/WNTBnCx9YcqdCmGZY8/giphy.gif" alt="Chris Oortman">
+            </a>
+        </article>
+
+    </section>
+</main>
+
+<?php include '../inc/footer/footer.php'; ?>
+
+<!-- SCRIPTS -->
+<?php include '../inc/footer/scripts.php'; ?>
+<!-- END SCRIPTS -->
+
+</body>
+</html>
