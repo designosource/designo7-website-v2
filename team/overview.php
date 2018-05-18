afae6fb936bcc33696aa939c8d6963198125d1de
ChrisOortman <oortmanchris@hotmail.com>
2018-05-18 13:56:30 +0200

    grid overview fixes

--- team/overview.php
+++ team/overview.php
@@ -36,17 +36,61 @@
 <main>
     <section class="page-container">
         <div class="previous-page">
-    	
-    			<a href="../index.php#team"><img src="../img/arrow-right-dark.svg" alt="icon previous page"><span>vorige pagina</span></a>
-    	
-    		</div>
+
+            <a href="../index.php#team"><img src="../img/arrow-right-dark.svg" alt="icon previous page"><span>vorige pagina</span></a>
+
+        </div>
 
         <h1>Meet the team</h1>
 
         <article class="overview" id="members-grid">
-            <a href="#">
+            <!--TODO: loop all members in grid-->
+            <a href="#" class="grid-item">
                 <img src="https://media.giphy.com/media/WNTBnCx9YcqdCmGZY8/giphy.gif" alt="Chris Oortman">
             </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/eSQKNSmg07dHq/giphy.gif" alt="Sander Van Hullebusch">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/8MObiTsZrFlTi/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/jzaZ23z45UxK8/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/l3V0lsGtTMSB5YNgc/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/110F1JFzWKtiA8/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/wqb5K5564JSlW/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/Zw133sEVc0WXK/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/Pjs1kqtH1KTaU/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/XOXdQszYm4I3m/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/wAxlCmeX1ri1y/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/PuKn7JfalRGnu/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/l0HlFZ3c4NENSLQRi/giphy.gif" alt="Hanna Walgrave">
+            </a>
+            <a href="#" class="grid-item">
+                <img src="https://media.giphy.com/media/xT9IgzUuC5Ss6ZnTEs/giphy.gif" alt="Hanna Walgrave">
+            </a>
+
         </article>
 
     </section>
