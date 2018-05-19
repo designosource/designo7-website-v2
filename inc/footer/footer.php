0d0b972f54f8e25e714c08e43f95ba0d13451412
ChrisOortman <oortmanchris@hotmail.com>
2018-05-19 21:33:59 +0200

    footer css minor fix

--- inc/footer/footer.php
+++ inc/footer/footer.php
@@ -1,15 +1,15 @@
-<footer class="maxwidth">
-
-		<nav id="footer-left">
-				<a href="/">Home</a>
-				<a href="#">Team</a>
-				<a href="#">Cases</a>
-				<a href="#">Contact</a>
-		</nav>
-		<nav id="footer-right">
-				<a target="_blank" href="https://www.facebook.com/Designosource/">Facebook</a>
-				<a target="_blank" href="https://twitter.com/designosource">Twitter</a>
-				<a target="_blank" href="http://instagram.com/designosource">Instagram</a>
-		</nav>
-
+<footer>
+    <div class="footer maxwidth">
+        <nav id="footer-left">
+            <a href="/">Home</a>
+            <a href="#">Team</a>
+            <a href="#">Cases</a>
+            <a href="#">Contact</a>
+        </nav>
+        <nav id="footer-right">
+            <a target="_blank" href="https://www.facebook.com/Designosource/">Facebook</a>
+            <a target="_blank" href="https://twitter.com/designosource">Twitter</a>
+            <a target="_blank" href="http://instagram.com/designosource">Instagram</a>
+        </nav>
+    </div>
 </footer>
