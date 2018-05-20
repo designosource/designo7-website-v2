2831d44e134329bc61f7ca4ce902d385cfeda5f2
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-20 22:35:30 +0200

    Fixed footer links + index#team links

--- inc/footer/footer.php
+++ inc/footer/footer.php
@@ -1,10 +1,23 @@
 <footer>
+   
+  	<?php $url = $_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+		if (strpos($url, 'cases/') !== false) {
+    	$prefix = '../';
+		} else if (strpos($url, 'team/') !== false) {
+			$prefix = '../';
+		} else {
+			$prefix = '';
+		}
+	
+		?>
+   
     <div class="footer maxwidth">
         <nav id="footer-left">
-            <a href="/designo7v2/#home">Home</a>
-            <a href="/designo7v2/#team">Team</a>
-            <a href="/designo7v2/#cases">Cases</a>
-            <a href="/designo7v2/#contact">Contact</a>
+            <a href="<?php echo $prefix; ?>index.php#home">Home</a>
+            <a href="<?php echo $prefix; ?>index.php#team">Team</a>
+            <a href="<?php echo $prefix; ?>index.php#cases">Cases</a>
+            <a href="<?php echo $prefix; ?>index.php#contact">Contact</a>
         </nav>
         <nav id="footer-right">
             <a target="_blank" href="https://www.facebook.com/Designosource/">Facebook</a>
--- inc/index/team.php
+++ inc/index/team.php
@@ -4,10 +4,10 @@
     <article class="content-rightblock">
         <span class="page-number">02/</span>
         <h2>Team</h2>
-        <a title="Wat" href="/team/wat.php"><span>Wat</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
-				<a title="Wat doen wij?" href="/team/services.php"><span>Wat doen wij?</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
-				<a title="Werkwijze" href="/team/werkwijze.php"><span>Werkwijze</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
-				<a title="Meet the team" href="/team/"><span>Meet the team</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
+        <a title="Wat" href="team/wat.php"><span>Wat</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
+				<a title="Wat doen wij?" href="team/services.php"><span>Wat doen wij?</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
+				<a title="Werkwijze" href="team/werkwijze.php"><span>Werkwijze</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
+				<a title="Meet the team" href="team/"><span>Meet the team</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
     </article>
 	</div> <!-- /maxwidth -->
 </section>
