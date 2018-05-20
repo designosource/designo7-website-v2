1c24e34d2a51a38828c04ce6527c0a5e4d81a659
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-20 18:04:50 +0200

    Added member page (to be used for each individual) member

---
+++ team/member.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- GOOGLE ANALYTICS SCRIPT -->
+
+    <!-- END GOOGLE ANALYTICS SCRIPT -->
+
+    <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php include 'inc/head/meta_tags.php'; ?>
+
+    <title>Team | Services</title>
+
+    <!-- Font(s) for this template (Google font / font-awesome) -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          type="text/css">
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/cooper-hewitt" type="text/css"/>
+
+    <!-- Animate.css -->
+    <link href="../css/animate.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="../css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<main>
+
+	<section id="member">
+  	<div class="container big-grid-2 maxwidth">
+			<figure class="content-image overlay"></figure>
+			
+			<article class="member-information">
+				
+				<p class="member-quote">Here is some place to put an inspirational quote</p>
+				
+				<div class="member-socialmedia">
+					
+					<a href="#">Linkedin</a>
+					<a href="#">Email</a>
+					
+				</div>
+				
+			</article>
+
+			<div class="previous-page">
+
+				<a title="Vorige pagina" href="../index.php#team"><img src="../img/arrow-right-red.svg" alt="icon previous page"><span>vorige pagina</span></a>
+
+			</div>
+			
+			<h1>Sander<br>Van Hullebusch</h1>
+			
+			<article class="content-rightblock scrollbar">
+					<p class="member-nickname">Animation astronaut</p>
+					
+					<p>Nam dapibus nisl vitae elit fringilla rutrum. Aenean sollicitudin, erat a elementum rutrum, neque sem pretium metus, quis mollis nisl nunc et massa. Vestibulum sed metus in lorem tristique.</p>
+					<a title="Ontdek onze cases" href="#" class="btn">Ontdek onze cases<span class="arrow-right"></span></a>
+			</article>
+		</div> <!-- /maxwidth -->
+	</section>
+
+</main>
+
+<?php include '../inc/footer/footer.php'; ?>
+
+<!-- SCRIPTS -->
+<?php include '../inc/footer/scripts.php'; ?>
+<!-- END SCRIPTS -->
+
+</body>
+</html>
