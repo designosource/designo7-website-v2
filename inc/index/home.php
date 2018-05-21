0436255aa58f2359c37e6e9411e33ae90ec8def5
ChrisOortman <oortmanchris@hotmail.com>
2018-05-21 13:55:47 +0200

    intro index

--- inc/index/home.php
+++ inc/index/home.php
@@ -1,4 +1,13 @@
 <section id="home">
-	<div class="container maxwidth">
-	</div> <!-- /maxwidth -->
+    <div class="container maxwidth" id="home-grid">
+        <img src="img/designologo.svg" alt="Logo Designosource" id="home-logo">
+        <article id="home-content">
+            <h3>Wij zijn</h3>
+            <h1>Designosource</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dicta dolor in reprehenderit
+                tempore.</p>
+            <a href="cases/" title="Ga naar cases" class="btn">Ga naar cases<span class="arrow-right-white"></span></a>
+        </article>
+        <div id="home-line"></div>
+    </div> <!-- /maxwidth -->
 </section>
