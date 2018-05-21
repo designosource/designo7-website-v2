62de24767a4c3245f27f198878b942b399061021
ChrisOortman <oortmanchris@hotmail.com>
2018-05-21 14:32:12 +0200

    hover effect home button

--- inc/index/home.php
+++ inc/index/home.php
@@ -6,7 +6,7 @@
             <h1>Designosource</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dicta dolor in reprehenderit
                 tempore.</p>
-            <a href="cases/" title="Ga naar cases" class="btn">Ga naar cases<span class="arrow-right-white"></span></a>
+            <a href="cases/" title="Ga naar cases" class="btn-white">Ga naar cases<span class="arrow-right-white"></span></a>
         </article>
         <div id="home-line"></div>
     </div> <!-- /maxwidth -->
