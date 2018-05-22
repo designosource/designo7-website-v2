b43673da3bd39f462bee87c26bfd2ff0d0e32e3b
ChrisOortman <oortmanchris@hotmail.com>
2018-05-22 16:17:51 +0200

    fix includes

--- cases/detail.php
+++ cases/detail.php
@@ -10,7 +10,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <?php include 'inc/head/meta_tags.php'; ?>
+    <?php include '../inc/head/meta_tags.php'; ?>
 
     <title>Cases | Case</title>
 
