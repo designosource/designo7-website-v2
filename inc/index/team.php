46963dca6df6369b9e8538325389cc7af1c63185
ChrisOortman <oortmanchris@hotmail.com>
2018-05-23 21:28:24 +0200

    scroll reveal on homepage

--- inc/index/team.php
+++ inc/index/team.php
@@ -1,16 +1,16 @@
 <section id="team">
- 
+
 	<?php include 'inc/logo/logo.php'; ?>
- 
+
   <div class="container maxwidth">
     <figure class="content-image" id="team-image"></figure>
     <article class="content-rightblock">
         <span class="page-number">02/</span>
         <h2>Team</h2>
-        <a title="Wat" href="team/wat.php"><span>Wat</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
-				<a title="Wat doen wij?" href="team/services.php"><span>Wat doen wij?</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
-				<a title="Werkwijze" href="team/werkwijze.php"><span>Werkwijze</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
-				<a title="Meet the team" href="team/"><span>Meet the team</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
+        <a title="Wat" href="team/wat.php" class="links"><span>Wat</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
+				<a title="Wat doen wij?" href="team/services.php" class="links"><span>Wat doen wij?</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
+				<a title="Werkwijze" href="team/werkwijze.php" class="links"><span>Werkwijze</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
+				<a title="Meet the team" href="team/" class="links"><span>Meet the team</span><img src="img/arrow-right-red.svg" alt="icon previous page"></a>
     </article>
 	</div> <!-- /maxwidth -->
 </section>
--- js/javascript.js
+++ js/javascript.js
@@ -2,19 +2,23 @@
 window.sr = ScrollReveal();
 sr.reveal('#home-line', { duration: 500, origin: 'left', distance: '100px', delay: 500, viewFactor: 0.3 });
 sr.reveal('#home-logo', { duration: 500, origin: 'top', distance: '100%', delay: 1000, viewFactor: 0.3 });
-sr.reveal('#home-content', { duration: 500, origin: 'bottom', distance: '100px', delay: 1500, viewFactor: 0.3 });
+sr.reveal('#home-content', { duration: 500, origin: 'bottom', distance: '20vh', delay: 1500, viewFactor: 0.3 });
 // sr.reveal('#home-content h3', { duration: 500, origin: 'bottom', distance: '100px', delay: 2000, viewFactor: 0.3 });
 // sr.reveal('#home-content h1', { duration: 500, origin: 'bottom', distance: '100px', delay: 2000, viewFactor: 0.3 });
 // sr.reveal('#home-content p', { duration: 500, origin: 'bottom', distance: '100px', delay: 2500, viewFactor: 0.3 });
 // sr.reveal('#home-content .btn-white', { duration: 500, origin: 'bottom', distance: '100px', delay: 20000, viewFactor: 0.3 });
 
-sr.reveal('#team .content-image', { duration: 1000, origin: 'bottom', distance: '200px', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
-sr.reveal('#team .content-rightblock', { duration: 1000, origin: 'top', distance: '200px', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
+sr.reveal('#team .content-image', { duration: 1000, origin: 'bottom', distance: '50vh', scale: 1, opacity: 0, delay: 250, viewFactor: 0.5 });
+sr.reveal('#team .content-rightblock', { duration: 1000, origin: 'top', distance: '50vh', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
+// sr.reveal('#team span', { duration: 1000, origin: 'top', distance: '200px', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
+// sr.reveal('#team h2', { duration: 1000, origin: 'top', distance: '200px', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
+// sr.reveal('#team .link', { duration: 1000, origin: 'top', distance: '200px', scale: 1, opacity: 0, delay: 1000, viewFactor: 0.5 });
 
-sr.reveal('#cases .content-image', { duration: 1000, origin: 'bottom', distance: '200px', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
-sr.reveal('#cases .content-rightblock', { duration: 1000, origin: 'top', distance: '200px', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
 
-sr.reveal('#contact .content-image', { duration: 1000, origin: 'bottom', distance: '200px', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
-sr.reveal('#contact .content-rightblock', { duration: 1000, origin: 'top', distance: '200px', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
+sr.reveal('#cases .content-image', { duration: 1000, origin: 'bottom', distance: '50vh', scale: 1, opacity: 0, delay: 250, viewFactor: 0.5 });
+sr.reveal('#cases .content-rightblock', { duration: 1000, origin: 'top', distance: '50vh', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
+
+sr.reveal('#contact .content-image', { duration: 1000, origin: 'bottom', distance: '50vh', scale: 1, opacity: 0, delay: 250, viewFactor: 0.5 });
+sr.reveal('#contact .content-rightblock', { duration: 1000, origin: 'top', distance: '50vh', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
 
 sr.reveal('.overview .grid-item', { duration: 500, origin: 'bottom', distance: '100px', delay: 1500, viewFactor: 0.3 }, 100);
