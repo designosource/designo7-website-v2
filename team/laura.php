f419e1afff784f3a35e6ff1e1037b9db87570c17
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-28 18:20:29 +0200

    Added team member pages

---
+++ team/laura.php
@@ -0,0 +1,56 @@
+<?php
+
+	include '../inc/arrays/members.php';
+
+	// Get page name
+	$pagename = basename(__FILE__, '.php');
+
+	// Fetch corresponding member info
+	$memberinfo = $members[$pagename];
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- GOOGLE ANALYTICS SCRIPT -->
+
+    <!-- END GOOGLE ANALYTICS SCRIPT -->
+
+    <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php include '../inc/head/meta_tags.php'; ?>
+
+    <title>Team | <?php echo ucfirst($pagename); ?></title>
+    
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/cooper-hewitt" type="text/css"/>
+
+    <!-- Animate.css -->
+    <link href="../css/animate.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="../css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<?php include '../inc/logo/logo.php'; ?>
+<?php include '../inc/hiddengame/hiddengame.php'; ?>
+
+<main>
+
+	<?php include '../inc/team/member_content.php'; ?>
+
+</main>
+
+<?php include '../inc/footer/footer.php'; ?>
+
+<!-- SCRIPTS -->
+<?php include '../inc/footer/scripts.php'; ?>
+<!-- END SCRIPTS -->
+
+</body>
+</html>
