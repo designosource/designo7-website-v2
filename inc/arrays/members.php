257247d774871e36cceb89f238607cd4e182ebda
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-28 18:17:22 +0200

    Completed arrays cases & members

--- inc/arrays/members.php
+++ inc/arrays/members.php
@@ -3,27 +3,147 @@
 
 $members = array(
     "member" => array(
-        "Name" => "Sander Van Hullebusch",
-        "Shortname" => "Sander",
-        "Title" => "Gulp guardian",
-        "Image" => "https://media.giphy.com/media/eSQKNSmg07dHq/giphy.gif",
-        "Quote" => "Quote Of The Member",
+        "name" => "Sander Van Hullebusch",
+        "shortname" => "sander",
+        "title" => "Gulp guardian",
+        "image" => "https://media.giphy.com/media/eSQKNSmg07dHq/giphy.gif",
+        "quote" => "Quote Of The Member",
         "Linkedin" => "https://www.linkedin.com/in/member1/",
-        "Email" => "member@gmail.com",
-        "Description" => "Description Of The Member",
-        "Website" => "www.member1.be"
+        "email" => "sandervanhullebusch@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.sandervanhullebusch.be"
     ),
 
     "chris" => array(
-        "Name" => "Chris Oortman",
-        "Shortname" => "Chris",
-        "Title" => "Branding badass",
-        "Image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
-        "Quote" => "Wilt er iemand een pintje?",
-        "Linkedin" => "https://www.linkedin.com/in/member2/",
-        "Email" => "member@gmail.com",
-        "Description" => "Description Of The Member",
-        "Website" => "www.member2.be"
+        "name" => "Chris Oortman",
+        "shortname" => "chris",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.member2.be"
+    ),
+	
+		"hanna" => array(
+        "name" => "Hanna Walgrave",
+        "shortname" => "hanna",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.hannawalgrave.be"
+    ),
+	
+		"joris" => array(
+        "name" => "Joris Keirsmaekers",
+        "shortname" => "joris",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.member2.be"
+    ),
+	
+		"lino" => array(
+        "name" => "Lino Coltura",
+        "shortname" => "lino",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.linocoltura.be"
+    ),
+	
+		"lisa" => array(
+        "name" => "Lisa Wouters",
+        "shortname" => "lisa",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.lisawouters.be"
+    ),
+	
+		"caroline" => array(
+        "name" => "Caroline Boeykens",
+        "shortname" => "caroline",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.member2.be"
+    ),
+	
+		"chiara" => array(
+        "name" => "Chiara Faes",
+        "shortname" => "chiara",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.lisawouters.be"
+    ),
+	
+		"larissa" => array(
+        "name" => "Larissa Goethals",
+        "shortname" => "larissa",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.larissagoethals.be"
+    ),
+	
+		"laura" => array(
+        "name" => "Laura Peeters",
+        "shortname" => "laura",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.laurapeeters.be"
+    ),
+	
+		"pieterjan" => array(
+        "name" => "Pieterjan Van Saet",
+        "shortname" => "pieterjan",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "https://www.akxis.io/"
+    ),
+	
+		"risa" => array(
+        "name" => "Risa Somers",
+        "shortname" => "risa",
+        "title" => "Branding badass",
+        "image" => "https://media.giphy.com/media/8j3CTd8YJtAv6/giphy.gif",
+        "quote" => "Wilt er iemand een pintje?",
+        "linkedin" => "https://www.linkedin.com/in/member2/",
+        "email" => "member@gmail.com",
+        "description" => "Description Of The Member",
+        "website" => "http://www.lisawouters.be"
     )
 );
 
