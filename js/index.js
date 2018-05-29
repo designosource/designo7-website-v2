92b0b879ad82e01fed5aeacd2def0f8fb563ff00
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-29 09:21:52 +0200

    Smooth scroll was way faster as scrollify -> fix

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 				var target = $(this.hash);
 				target = target.length ? target : $('[name=' + this.hash.slice(1) +']'); 
 				if (target.length) {
-					$('html,body').animate({ scrollTop: (target.offset().top) }, 300);
+					$('html,body').animate({ scrollTop: (target.offset().top) }, 1000, "ease");
 				}
 			} 
 		}); 
