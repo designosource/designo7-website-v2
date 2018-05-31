49917756914e0e2209cb5ecb178bd02f0c2f8ce1
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-05-31 18:04:36 +0200

    Changed text 'Wat' to 'Wie zijn wij?'

--- inc/index/team.php
+++ inc/index/team.php
@@ -10,7 +10,7 @@
             <h2>Team</h2>
         </div>
         <div class="content-group">
-            <a title="Wat" href="team/wat.php" class="links"><span>Wat</span><img src="img/arrow-right-red.svg" alt="icon previous page" class="arrow"></a>
+            <a title="Wat" href="team/wat.php" class="links"><span>Wie zijn wij?</span><img src="img/arrow-right-red.svg" alt="icon previous page" class="arrow"></a>
             <a title="Wat doen wij?" href="team/services.php" class="links"><span>Wat doen wij?</span><img src="img/arrow-right-red.svg" alt="icon previous page" class="arrow"></a>
             <a title="Werkwijze" href="team/werkwijze.php" class="links"><span>Werkwijze</span><img src="img/arrow-right-red.svg" alt="icon previous page" class="arrow"></a>
             <a title="Meet the team" href="team/" class="links"><span>Meet the team</span><img src="img/arrow-right-red.svg" alt="icon previous page" class="arrow"></a>
