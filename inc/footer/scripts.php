065fd16d79073bdfddbc429e3621b5580f734cf6
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-06-02 23:41:11 +0200

    Same as this one - fix of script link

--- inc/footer/scripts.php
+++ inc/footer/scripts.php
@@ -2,5 +2,5 @@
 <script type="text/javascript" src="<?php echo $prefix; ?>js/index.js"></script>
 <script type="text/javascript" src="<?php echo $prefix; ?>js/slick-min.js"></script>
 <script type="text/javascript" src="<?php echo $prefix; ?>js/scroll-reveal-min.js"></script>
-<script type="text/javascript" src="<?php echo $prefix; ?>js/javascript-js.js"></script>
+<script type="text/javascript" src="<?php echo $prefix; ?>js/javascript-min.js"></script>
 <script type="text/javascript" src="<?php echo $prefix; ?>js/jquery.scrollify-min.js"></script>
