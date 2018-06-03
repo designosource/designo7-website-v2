e942be257d9ef5adab58c95fad97b74713c04388
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-06-03 14:57:35 +0200

    Old browser template(s) for team pages

---
+++ inc/team/wat-2.php
@@ -0,0 +1,28 @@
+<section class="memberpage" id="wat">
+   
+   	<div class="container big-grid maxwidth">
+			<figure class="content-image overlay"></figure>
+			<figure class="content-image pop-up-image"></figure>
+
+			<div class="previous-next-page">
+
+				<a title="Services pagina" href="services.php"><img src="../img/arrow-right-red.svg" alt="icon previous page"><span>Services</span></a>
+				
+				<a title="Overzicht" href="../team/"><img src="../img/team-icon-dark.svg" alt="icon team page" onmouseover="$(this).attr('src','../img/team-icon-red.svg').css('transition', '0.4s');" onmouseleave="$(this).attr('src','../img/team-icon-dark.svg').css('transition', '0.4s');" ></a>
+				
+				<a title="Werkwijze pagina" href="werkwijze.php"><span>Services</span><img src="../img/arrow-right-red.svg" alt="icon previous page"></a>
+
+			</div>
+
+			<h1>Designosource</h1>
+
+			<article class="content-rightblock scrollbar">
+					<p>Wij zijn een creative agency bestaande uit 12 studenten Interactive Multimedia Design. Gaande van development nerds tot interface queens en van design hipsters tot sushi lovers, vormen wij een uniek en vooral enthousiast team.</p>
+					<p>Benieuwd naar wat wij al gerealiseerd hebben?</p>
+					<a title="Ontdek onze cases" href="../cases/" class="btn">Ontdek onze cases<span class="arrow-right"></span></a>
+			</article>
+			
+			<div class="fixed-bottom-mobile-navigation"></div>
+			
+    </div> <!-- /maxwidth -->
+	</section>
--- team/werkwijze.php
+++ team/werkwijze.php
@@ -37,38 +37,7 @@
 
 <main>
 
-	<section class="memberpage" id="werkwijze">
-  	<div class="container big-grid maxwidth">
-			<figure class="content-image overlay"></figure>
-			<figure class="content-image pop-up-image"></figure>
-
-			<div class="previous-next-page">
-
-				<a title="Wat pagina" href="wat.php"><img src="../img/arrow-right-red.svg" alt="icon previous page"><span>Wat</span></a>
-				
-				<a title="Overzicht" href="../team/"><img src="../img/team-icon-dark.svg" alt="icon team page" onmouseover="$(this).attr('src','../img/team-icon-red.svg').css('transition', '0.4s');" onmouseleave="$(this).attr('src','../img/team-icon-dark.svg').css('transition', '0.4s');" ></a>
-				
-				<a title="Services pagina" href="services.php"><span>Services</span><img src="../img/arrow-right-red.svg" alt="icon previous page"></a>
-
-			</div>
-
-			<h1>Werkwijze</h1>
-
-			<article class="content-rightblock scrollbar">
-					<p>Een uniek agency als dat van Designosource vraagt ook om een unieke aanpak. Elk project wordt door een kleiner maar gespecialiseerder team met evenveel Dino-care behandeld.</p>
-					<ol id="list">
-						<li>1. Opstart meeting</li>
-						<li>2. Projectmanagement</li>
-						<li>3. Uitwerking</li>
-						<li>4. Opvolging</li>
-					</ol>
-					<a title="Ontdek onze cases" href="../cases/" class="btn">Ontdek onze cases<span class="arrow-right"></span></a>
-			</article>
-			
-			<div class="fixed-bottom-mobile-navigation"></div>
-			
-		</div> <!-- /maxwidth -->
-	</section>
+	<?php include '../inc/team/werkwijze' . $version . '.php'; ?>
 
 </main>
 
