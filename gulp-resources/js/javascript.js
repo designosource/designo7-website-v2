1e3186f8f9fd8b6423fbd6d3359861a79513757d
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-06-06 10:10:04 +0200

    Animations & other stupid stuff...

--- gulp-resources/js/javascript.js
+++ gulp-resources/js/javascript.js
@@ -24,9 +24,11 @@ if (window.matchMedia('(max-width: 720px)').matches) {
     sr.reveal('#contact .content-image', { duration: 1000, origin: 'bottom', distance: '20vh', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
     sr.reveal('#contact .content-group', { duration: 1000, origin: 'bottom', distance: '20vh', scale: 1, opacity: 0, delay: 750, viewFactor: 0.5 });
 	
-	sr.reveal('.memberpage .content-image.overlay', { duration: 1000, origin: 'bottom', distance: '100px', scale: 1, opacity: 1, viewFactor: 0.5 });
-	sr.reveal('.memberpage h1', { duration: 1000, origin: 'bottom', distance: '100px', scale: 1, opacity: 1, viewFactor: 0.5 });
-	sr.reveal('.memberpage article.content-rightblock', { duration: 1000, origin: 'bottom', distance: '100px', scale: 1, opacity: 0, delay: 250, viewFactor: 0.5 });
+	sr.reveal('.memberpage .coloured-background', { duration: 750, origin: 'top', distance: '100px', scale: 1, opacity: 0, delay: 0, viewFactor: 0.5 });
+	sr.reveal('.memberpage .content-image.overlay', { duration: 750, origin: 'top', distance: '100px', scale: 1, opacity: 0, delay: 0, viewFactor: 0.5 });
+	sr.reveal('.memberpage .content-image.pop-up-image', { duration: 750, origin: 'bottom', distance: '100px', scale: 1, opacity: 0, delay: 250, viewFactor: 0.5 });
+	sr.reveal('.memberpage h1', { duration: 750, origin: 'bottom', distance: '50px', scale: 1, opacity: 0, delay: 500,  viewFactor: 0.5 });
+	sr.reveal('.memberpage article.content-rightblock', { duration: 750, origin: 'bottom', distance: '100px', scale: 1, opacity: 0, delay: 750, viewFactor: 0.2 });
 	
 	sr.reveal('.casepage .slick-container.pop-up-image', { duration: 1000, origin: 'bottom', distance: '100px', scale: 1, opacity: 0, viewFactor: 0.5 });
 	sr.reveal('.casepage h1', { duration: 1000, origin: 'bottom', distance: '100px', scale: 1, opacity: 0, delay: 250, viewFactor: 0.5 });
@@ -60,4 +62,6 @@ if (window.matchMedia('(max-width: 720px)').matches) {
 	sr.reveal('.casepage h1', { duration: 1000, origin: 'right', distance: '20vw', scale: 1, opacity: 0, delay: 250, viewFactor: 0.5 });
 	sr.reveal('.casepage article.content-rightblock', { duration: 1000, origin: 'right', distance: '20vw', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
 	sr.reveal('.casepage .previous-next-page', { duration: 1000, origin: 'right', distance: '20vw', scale: 1, opacity: 0, delay: 500, viewFactor: 0.5 });
+	
+	sr.reveal('footer', { duration: 500, origin: 'bottom', distance: '100%', scale: 1, opacity: 0, delay: 1000, viewFactor: 0.5 });
 }
