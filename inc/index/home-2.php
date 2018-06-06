d3082044a62607fabce6ed58157d92e1f85fc7d8
ChrisOortman <oortmanchris@hotmail.com>
2018-06-06 13:59:04 +0200

    home section css old browser

--- inc/index/home-2.php
+++ inc/index/home-2.php
@@ -1,19 +1,23 @@
 <section id="home" class="page">
-    <div class="container maxwidth" id="home-grid">
-        <img src="img/designologo.svg" alt="Logo Designosource" id="home-logo">
-        <article id="home-content">
-            <h3>Wij zijn</h3>
-            <h1>Designosource</h1>
-            <p>Gedreven, jong en gepassioneerd! Een lijstje mooie woorden? Ja dat ook, maar vooral de stempel die wij drukken op de 7e generatie Designosource.</p>
-            <a href="cases/" title="Ga naar cases" class="btn-white">Ga naar cases<span class="arrow-right-white"></span></a>
-        </article>
-        <div id="home-line"></div>
-        <!--<img src="img/scroll-icon.svg" alt="Scroll" id="scroll">-->
-        
-        <a title="Scroll to team" id="scroll" href="#team">
-        	<div>
-        		<div class="scroll-line"></div>
-        	</div>
-        </a>
+    <div class="container-home-flex maxwidth" id="home-grid">
+        <div id="flex-row-1">
+            <img src="img/designologo.svg" alt="Logo Designosource" id="home-logo">
+        </div>
+        <div id="flex-row-2">
+            <div id="home-line"></div>
+            <article id="home-content">
+                <h3>Wij zijn</h3>
+                <h1>Designosource</h1>
+                <p>Gedreven, jong en gepassioneerd! Een lijstje mooie woorden? Ja dat ook, maar vooral de stempel die wij drukken op de 7e generatie Designosource.</p>
+                <a href="cases/" title="Ga naar cases" class="btn-white">Ga naar cases<span class="arrow-right-white"></span></a>
+            </article>
+        </div>
+        <div id="flex-row-3">
+            <a title="Scroll to team" id="scroll" href="#team">
+                <div>
+                    <div class="scroll-line"></div>
+                </div>
+            </a>
+        </div>
     </div> <!-- /maxwidth -->
 </section>
