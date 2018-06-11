39e089dd037a6d8348f98cb2795da303ab7aefa3
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2018-06-11 10:29:51 +0200

    Ho Ho Hooooo :santa:

--- inc/head/meta_tags_wat.php
+++ inc/head/meta_tags_wat.php
@@ -5,15 +5,15 @@
 <meta name="designer" content="Designosource">
 <meta name="reply-to" content="hello@designosource.be">
 <meta name="owner" content="Designosource">
-<meta name="url" content="http://www.sandervanhullebusch.be/designo7v2/team/wat.php">
-<meta name="url" content="https://www.sandervanhullebusch.be/designo7v2/team/wat.php">
-<meta name="identifier-URL" content="http://www.sandervanhullebusch.be/designo7v2/">
+<meta name="url" content="http://www.designosource.be/team/wat.php">
+<meta name="url" content="https://www.designosource.be/team/wat.php">
+<meta name="identifier-URL" content="http://www.designosource.be/">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta property="og:title" content="Designosouce | Wat doen wij?">
 <meta property="og:type" content="website">
-<meta property="og:url" content="http://www.sandervanhullebusch.be/designo7v2/team/wat.php">
-<meta property="og:image" content="http://www.sandervanhullebusch.be/designo7v2/img/meta/og-team.jpg">
-<meta property="og:image:secure_url" content="https://www.sandervanhullebusch.be/designo7v2/img/meta/og-team.jpg">
+<meta property="og:url" content="http://www.designosource.be/team/wat.php">
+<meta property="og:image" content="http://www.designosource.be/img/meta/og-team.jpg">
+<meta property="og:image:secure_url" content="https://www.designosource.be/img/meta/og-team.jpg">
 <meta property="og:image:alt" content="Designosource | OG image wat page">
 <meta property="og:image:width" content="1200">
 <meta property="og:image:height" content="627">
@@ -25,9 +25,9 @@
 <meta name="twitter:creator" content="@designosource">
 <meta name="twitter:title" content="Designosource | Wie zijn we?">
 <meta name="twitter:description" content="Designosource is een creative agency bestaande uit 12 studenten Interactive Multimedia Design. Gaande van development nerds tot interface queens en van design hipsters tot sushi lovers, vormen zij een uniek en vooral enthousiast team.">
-<meta name="twitter:image" content="http://www.sandervanhullebusch.be/designo7v2/img/meta/twitter-team.jpg">
-<link rel="apple-touch-icon" sizes="152x152" href="http://www.sandervanhullebusch.be/designo7v2/img/meta/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="http://www.sandervanhullebusch.be/designo7v2/img/meta/apple-icon-180x180.png">
+<meta name="twitter:image" content="http://www.designosource.be/img/meta/twitter-team.jpg">
+<link rel="apple-touch-icon" sizes="152x152" href="http://www.designosource.be/img/meta/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="http://www.designosource.be/img/meta/apple-icon-180x180.png">
 <meta name="apple-mobile-web-app-title" content="Designosource | Wie zijn we?">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="default-translucent">
